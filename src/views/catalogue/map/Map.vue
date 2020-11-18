98218f5c0f5cb0f237ef89ee2e3b5560942e90f8
papizzo <papizzo@istat.it>
2020-11-18 10:30:25 +0100

    problem plugin choropleth  installato ma non funziona?

--- src/views/catalogue/map/Map.vue
+++ src/views/catalogue/map/Map.vue
@@ -1,152 +1,86 @@
-<!--template>
-  <div class="row">
-    <div class="col-sm-12 col-md-12">
-      <div class="card">
-        <header class="card-header">
-          Map
-          <div class="card-header-actions">
-            <router-link tag="a" :to="{ name: 'Map' }">
-              <add-icon />
-            </router-link>
-          </div>
-        </header>
-        <CCardBody>
-          <l-map style="height: 650px" :zoom="zoom" :center="center">
-            <l-tile-layer :url="url"></l-tile-layer>
-            <l-marker :lat-lng="markerLatLng"></l-marker>
-            <l-geo-json :geojson="geojson"></l-geo-json>
-          </l-map>
-        </CCardBody>
-      </div>
-    </div>
-  </div>
-</template>
-
-<script>
-import { mapGetters } from "vuex";
-import { LMap, LTileLayer, LMarker, LGeoJson } from "vue2-leaflet";
-
-export default {
-  components: {
-    LMap,
-    LTileLayer,
-    LMarker,
-    LGeoJson
-  },
-  data() {
-    return {
-      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-      zoom: 3,
-      center: [47.41322, -1.219482],
-      markerLatLng: [47.41322, -1.219482],
-      geojson: null
-    };
-  },
-  computed: {
-    ...mapGetters("geomap", ["geomaps"])
-  },
-  async created() {
-    const response = await this.$store.dispatch("geomaps/findAll");
-    this.geojson = await response;
-  }
-};
-</script-->
 <template>
-  <div>
-    <div>
-      <span v-if="loading">Loading...</span>
-      <label for="checkbox">GeoJSON Visibility</label>
-      <input id="checkbox" v-model="show" type="checkbox" />
-      <label for="checkboxTooltip">Enable tooltip</label>
-      <input id="checkboxTooltip" v-model="enableTooltip" type="checkbox" />
-      <input v-model="fillColor" type="color" />
-      <br />
-    </div>
-    <l-map :zoom="zoom" :center="center" style="height: 500px; width: 100%">
-      <l-tile-layer :url="url" :attribution="attribution" />
+  <div id="app">
+    <l-map
+      :center="[-23.752961, -57.854357]"
+      :zoom="6"
+      style="height: 500px;"
+      :options="mapOptions"
+    >
       <l-geo-json
         v-if="show"
         :geojson="geojson"
         :options="options"
         :options-style="styleFunction"
       />
-      <l-marker :lat-lng="marker" />
+      <l-choropleth-layer
+        :data="pyDepartmentsData"
+        titleKey="department_name"
+        idKey="department_id"
+        :value="value"
+        :extraValues="extraValues"
+        geojsonIdKey="dpto"
+        :geojson="paraguayGeojson"
+        :colorScale="colorScale"
+      >
+        <template slot-scope="props">
+          <l-info-control
+            :item="props.currentItem"
+            :unit="props.unit"
+            title="Department"
+            placeholder="Hover over a department"
+          />
+          <l-reference-chart
+            title="Girls school enrolment"
+            :colorScale="colorScale"
+            :min="props.min"
+            :max="props.max"
+            position="topright"
+          />
+        </template>
+      </l-choropleth-layer>
     </l-map>
   </div>
 </template>
 
 <script>
-import { latLng } from "leaflet";
-import { LMap, LTileLayer, LMarker, LGeoJson } from "vue2-leaflet";
+import { InfoControl, ReferenceChart, ChoroplethLayer } from "vue-choropleth";
+// eslint-disable-next-line no-unused-vars
+import { geojson } from "./data/py-departments-geojson";
+import paraguayGeojson from "./data/paraguay.json";
+import { pyDepartmentsData } from "./data/py-departments-data";
+import { LMap, LGeoJson } from "vue2-leaflet";
+//import { LMap, LTileLayer, LControl, LMarker, LGeoJson } from "vue2-leaflet";
+
 export default {
-  name: "geomap",
+  name: "app",
   components: {
     LMap,
-    LTileLayer,
     LGeoJson,
-    LMarker
+    "l-info-control": InfoControl,
+    "l-reference-chart": ReferenceChart,
+    "l-choropleth-layer": ChoroplethLayer
   },
   data() {
     return {
-      loading: false,
-      show: true,
-      enableTooltip: true,
-      zoom: 3,
-      center: [45.861347, 57.405578],
-      /*center: latLng(45.861347, 57.405578),*/
-      geojson: null,
-      fillColor: "#e4ce7f",
-      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-      attribution:
-        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      marker: latLng(41.86956082699455, 12.436523437500002)
+      pyDepartmentsData,
+      paraguayGeojson,
+      colorScale: ["e7d090", "e9ae7b", "de7062"],
+      value: {
+        key: "amount_w",
+        metric: "% girls"
+      },
+      extraValues: [
+        {
+          key: "amount_m",
+          metric: "% boys"
+        }
+      ],
+      mapOptions: {
+        attributionControl: false
+      },
+      currentStrokeColor: "3d3213",
+      geojson: null
     };
-  },
-  computed: {
-    options() {
-      return {
-        onEachFeature: this.onEachFeatureFunction
-      };
-    },
-    styleFunction() {
-      return () => {
-        return {
-          weight: 2,
-          //color: "#ECEFF1",
-          opacity: 1,
-          fillOpacity: 0.6
-        };
-      };
-    },
-    onEachFeatureFunction() {
-      if (!this.enableTooltip) {
-        return () => {};
-      }
-      //return (feature, layer,style) => {
-      return (feature, layer) => {
-        layer.bindTooltip(
-          "<div>" +
-            "<div>" +
-            feature.properties.continent +
-            " </div>" +
-            "<div>" +
-            feature.properties.subregion +
-            " </div>" +
-            "<div> " +
-            feature.properties.sovereignt +
-            " </div>" +
-            "</div>",
-          { permanent: false, sticky: true }
-        );
-        console.log(
-          "Population " +
-            feature.properties.pop_est +
-            ", color: " +
-            getColor(feature.properties.pop_est)
-        );
-        layer.options.fillColor = getColor(feature.properties.pop_est);
-      };
-    }
   },
   async created() {
     this.loading = true;
@@ -156,22 +90,16 @@ export default {
     this.loading = false;
   }
 };
-
-function getColor(d) {
-  return d > 1000
-    ? "#800026"
-    : d > 500
-    ? "#BD0026"
-    : d > 200
-    ? "#E31A1C"
-    : d > 100
-    ? "#FC4E2A"
-    : d > 50
-    ? "#FD8D3C"
-    : d > 20
-    ? "#FEB24C"
-    : d > 10
-    ? "#FED976"
-    : "#FFEDA0";
-}
 </script>
+<style>
+@import "../node_modules/leaflet/dist/leaflet.css";
+body {
+  background-color: #e7d090;
+  margin-left: 100px;
+  margin-right: 100px;
+}
+
+#map {
+  background-color: #eee;
+}
+</style>
---
+++ src/views/catalogue/map/MapOkMap.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12 col-md-12">
+      <div class="card">
+        <header class="card-header">
+          Map
+          <div class="card-header-actions">
+            <span v-if="loading">Loading...</span>
+            <router-link tag="a" :to="{ name: 'Map' }">
+              <add-icon />
+            </router-link>
+          </div>
+        </header>
+        <CCardBody>
+          <div>
+            <l-map
+              :zoom="zoom"
+              :center="center"
+              style="height: 650px; width: 100%"
+            >
+              <l-tile-layer :url="url" :attribution="attribution" />
+              <l-geo-json
+                v-if="show"
+                :geojson="geojson"
+                :options="options"
+                :options-style="styleFunction"
+              />
+              <l-marker :lat-lng="marker" />
+              <l-control position="bottomright">uuuuuuuuuuuuuuuuuu</l-control>
+            </l-map>
+          </div>
+        </CCardBody>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { latLng } from "leaflet";
+import { LMap, LTileLayer, LControl, LMarker, LGeoJson } from "vue2-leaflet";
+export default {
+  name: "geomap",
+  components: {
+    LMap,
+    LTileLayer,
+    LControl,
+    LGeoJson,
+    LMarker
+  },
+  data() {
+    return {
+      loading: false,
+      show: true,
+      enableTooltip: true,
+      zoom: 3,
+      center: [45.861347, 57.405578],
+      /*center: latLng(45.861347, 57.405578),*/
+      geojson: null,
+      fillColor: "#e4ce7f",
+      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      marker: latLng(41.86956082699455, 12.436523437500002)
+    };
+  },
+  computed: {
+    options() {
+      return {
+        onEachFeature: this.onEachFeatureFunction
+      };
+    },
+    styleFunction() {
+      return () => {
+        return {
+          weight: 2,
+          opacity: 1,
+          fillOpacity: 0.6
+        };
+      };
+    },
+    onEachFeatureFunction() {
+      if (!this.enableTooltip) {
+        return () => {};
+      }
+      return (feature, layer) => {
+        layer.bindTooltip(
+          "<div>" +
+            "<div>" +
+            feature.properties.continent +
+            " </div>" +
+            "<div>" +
+            feature.properties.subregion +
+            " </div>" +
+            "<div> " +
+            feature.properties.sovereignt +
+            " </div>" +
+            "<div> " +
+            feature.properties.pop_est +
+            "</div>",
+          "</div>",
+          { permanent: false, sticky: true }
+        );
+        console.log(
+          "Population " +
+            feature.properties.pop_est +
+            ", color: " +
+            getColor(feature.properties.pop_est)
+        );
+        layer.options.fillColor = getColor(feature.properties.pop_est);
+        layer.options.color = getColor(feature.properties.pop_est);
+      };
+    }
+  },
+  async created() {
+    this.loading = true;
+    const response = await fetch("http://localhost:3000/countries");
+    const data = await response.json();
+    this.geojson = data;
+    this.loading = false;
+  }
+};
+function getColor(d) {
+  d = d / 2000000;
+  return d > 1000
+    ? "#800026"
+    : d > 500
+    ? "#BD0026"
+    : d > 200
+    ? "#E31A1C"
+    : d > 100
+    ? "#FC4E2A"
+    : d > 50
+    ? "#FD8D3C"
+    : d > 20
+    ? "#FEB24C"
+    : d > 10
+    ? "#FED976"
+    : "#FFEDA0";
+}
+</script>

d343ad1a6d06f0de02e735b7d33046fd845bb0ca
papizzo <papizzo@istat.it>
2020-11-19 12:37:52 +0100

    legend + info

--- src/views/catalogue/map/Map.vue
+++ src/views/catalogue/map/Map.vue
@@ -217,4 +217,16 @@ export default {
   margin-right: 8px;
   opacity: 0.7;
 }
+.info {
+  padding: 6px 8px;
+  font: 18px/20px sans-serif;
+  background: white;
+  background: rgba(255, 255, 255, 0.8);
+  box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+  border-radius: 5px;
+}
+.info h4 {
+  margin: 0 0 5px;
+  color: #777;
+}
 </style>

9d04006339a2b8b97593e5c8dc9c7dd7ba4fed9a
papizzo <papizzo@istat.it>
2020-12-17 22:01:26 +0100

    common update

--- src/views/catalogue/map/Map.vue
+++ src/views/catalogue/map/Map.vue
@@ -87,8 +87,16 @@ vue/no-unused-components */
   </div>
 </template>
 <script>
+import {
+  Map,
+  scale,
+  mouseover,
+  mouseout,
+  buildInfo,
+  getColor,
+  buildLegend
+} from "@/common/map";
 import { mapGetters } from "vuex";
-import { Map } from "@/common";
 import { latLng } from "leaflet";
 import {
   LMap,
@@ -181,67 +189,12 @@ export default {
     }
   },
   methods: {
-    scale(d) {
-      const min = 1;
-      const factor = 5;
-      const zoomFactor = this.zoom >= 5 ? 1 : this.zoom / 10; // adjust divisor for best optics
-      return Math.floor(Math.log(d) * factor * zoomFactor) + min;
-    },
-    mouseover: function({ target }) {
-      target.setStyle({
-        weight: this.currentStrokeWidth,
-        color: `#${this.currentStrokeColor}`,
-        dashArray: ""
-      });
-      this.info = this.buildInfo(target.feature);
-    },
-    mouseout: function({ target }) {
-      target.setStyle({
-        weight: this.strokeWidth,
-        color: `#${this.strokeColor}`,
-        dashArray: ""
-      });
-      this.info = this.buildInfo("");
-    },
-    buildInfo: function(props) {
-      var div = props
-        ? "State of " + "<b>" + props.properties.display_name + "</b><br/>"
-        : "Hover over a state";
-      return div;
-    },
-    getColor: function(d) {
-      return d > 10000000
-        ? "#800026"
-        : d > 500000
-        ? "#BD0026"
-        : d > 200000
-        ? "#E31A1C"
-        : d > 100000
-        ? "#FC4E2A"
-        : d > 50000
-        ? "#FD8D3C"
-        : d > 20000
-        ? "#FEB24C"
-        : d > 10000
-        ? "#FED976"
-        : "#FFEDA0";
-    },
-    buildLegend: function() {
-      this.legend.title = "State";
-      var grades = [10000, 20000, 50000, 100000, 200000, 500000, 1000000],
-        from,
-        to;
-
-      for (var i = 0; i < grades.length; i++) {
-        from = grades[i];
-        to = grades[i + 1];
-        this.legend.series.push({
-          color: this.getColor(from + 1),
-          fromNumber: from,
-          toNumber: to
-        });
-      }
-    }
+    scale,
+    mouseover,
+    mouseout,
+    buildInfo,
+    getColor,
+    buildLegend
   },
   created() {
     this.loading = true;

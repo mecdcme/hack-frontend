d52b3706b0776f19484f73dddffb709c1cb01946
mauroIstat <mbruno@istat.it>
2021-03-01 10:33:37 +0100

    New environment

--- json-server/countries.js
+++ json-server/countries.js
@@ -1,287 +1,323 @@
 module.exports = [
-  {    "country":"AD",
-       "coordinates":{
-              "latitude":"42.546245","longitude":"1.601554"
-         },
-         "name":"Andorra"  
- },
   {
-      "country":"AL",
-       "coordinates":{
-              "latitude":"41.153332","longitude":"20.168331"
-         },
-         "name":"Albania"  
- },
+    country: "AD",
+    coordinates: {
+      latitude: "42.546245",
+      longitude: "1.601554"
+    },
+    name: "Andorra"
+  },
   {
-      "country":"AT",
-       "coordinates":{
-              "latitude":"47.516231","longitude":"14.550072"
-         },
-         "name":"Austria"  
- },
- 
+    country: "AL",
+    coordinates: {
+      latitude: "41.153332",
+      longitude: "20.168331"
+    },
+    name: "Albania"
+  },
   {
-      "country":"BA",
-       "coordinates":{
-              "latitude":"43.915886","longitude":"17.679076"
-         },
-         "name":"Bosnia and Herzegovina"  
- },
- 
-  {
-      "country":"BE",
-       "coordinates":{
-              "latitude":"50.503887","longitude":"4.469936"
-         },
-         "name":"Belgium"  
- },
+    country: "AT",
+    coordinates: {
+      latitude: "47.516231",
+      longitude: "14.550072"
+    },
+    name: "Austria"
+  },
 
   {
-      "country":"BG",
-       "coordinates":{
-              "latitude":"42.733883","longitude":"25.48583"
-         },
-         "name":"Bulgaria"  
- },
- 
+    country: "BA",
+    coordinates: {
+      latitude: "43.915886",
+      longitude: "17.679076"
+    },
+    name: "Bosnia and Herzegovina"
+  },
+
   {
-      "country":"CH",
-       "coordinates":{
-              "latitude":"46.818188","longitude":"8.227512"
-         },
-         "name":"Switzerland"  
- },
- 
+    country: "BE",
+    coordinates: {
+      latitude: "50.503887",
+      longitude: "4.469936"
+    },
+    name: "Belgium"
+  },
+
   {
-      "country":"CZ",
-       "coordinates":{
-              "latitude":"49.817492","longitude":"15.472962"
-         },
-         "name":"Czech Republic"  
- },
+    country: "BG",
+    coordinates: {
+      latitude: "42.733883",
+      longitude: "25.48583"
+    },
+    name: "Bulgaria"
+  },
+
   {
-      "country":"DE",
-       "coordinates":{
-              "latitude":"51.165691","longitude":"10.451526"
-         },
-         "name":"Germany"  
- },
+    country: "CH",
+    coordinates: {
+      latitude: "46.818188",
+      longitude: "8.227512"
+    },
+    name: "Switzerland"
+  },
+
   {
-      "country":"DK",
-       "coordinates":{
-              "latitude":"56.26392","longitude":"9.501785"
-         },
-         "name":"Denmark"  
- },
- 
+    country: "CZ",
+    coordinates: {
+      latitude: "49.817492",
+      longitude: "15.472962"
+    },
+    name: "Czech Republic"
+  },
   {
-      "country":"ES",
-       "coordinates":{
-              "latitude":"40.463667","longitude":"-3.74922"
-         },
-         "name":"Spain"  
- },
-  
+    country: "DE",
+    coordinates: {
+      latitude: "51.165691",
+      longitude: "10.451526"
+    },
+    name: "Germany"
+  },
   {
-      "country":"FI",
-       "coordinates":{
-              "latitude":"61.92411","longitude":"25.748151"
-         },
-         "name":"Finland"  
- },
- 
+    country: "DK",
+    coordinates: {
+      latitude: "56.26392",
+      longitude: "9.501785"
+    },
+    name: "Denmark"
+  },
+
   {
-      "country":"FR",
-       "coordinates":{
-              "latitude":"46.227638","longitude":"2.213749"
-         },
-         "name":"France"  
- },
- 
+    country: "ES",
+    coordinates: {
+      latitude: "40.463667",
+      longitude: "-3.74922"
+    },
+    name: "Spain"
+  },
+
   {
-      "country":"GB",
-       "coordinates":{
-              "latitude":"55.378051","longitude":"-3.435973"
-         },
-         "name":"United Kingdom"  
- },
- 
+    country: "FI",
+    coordinates: {
+      latitude: "61.92411",
+      longitude: "25.748151"
+    },
+    name: "Finland"
+  },
+
   {
-      "country":"GR",
-       "coordinates":{
-              "latitude":"39.074208","longitude":"21.824312"
-         },
-         "name":"Greece"  
- },
- 
+    country: "FR",
+    coordinates: {
+      latitude: "46.227638",
+      longitude: "2.213749"
+    },
+    name: "France"
+  },
+
   {
-      "country":"HR",
-       "coordinates":{
-              "latitude":"45.1","longitude":"15.2"
-         },
-         "name":"Croatia"  
- },
- 
+    country: "GB",
+    coordinates: {
+      latitude: "55.378051",
+      longitude: "-3.435973"
+    },
+    name: "United Kingdom"
+  },
+
   {
-      "country":"HU",
-       "coordinates":{
-              "latitude":"47.162494","longitude":"19.503304"
-         },
-         "name":"Hungary"  
- },
- 
+    country: "GR",
+    coordinates: {
+      latitude: "39.074208",
+      longitude: "21.824312"
+    },
+    name: "Greece"
+  },
+
   {
-      "country":"IE",
-       "coordinates":{
-              "latitude":"53.41291","longitude":"-8.24389"
-         },
-         "name":"Ireland"  
- },
-  
+    country: "HR",
+    coordinates: {
+      latitude: "45.1",
+      longitude: "15.2"
+    },
+    name: "Croatia"
+  },
+
   {
-      "country":"IS",
-       "coordinates":{
-              "latitude":"64.963051","longitude":"-19.020835"
-         },
-         "name":"Iceland"  
- },
+    country: "HU",
+    coordinates: {
+      latitude: "47.162494",
+      longitude: "19.503304"
+    },
+    name: "Hungary"
+  },
+
   {
-      "country":"IT",
-       "coordinates":{
-        "latitude":"41.89277044","longitude":"12.48366722"
-         },
-         "name":"Italy"  
- },
-  
+    country: "IE",
+    coordinates: {
+      latitude: "53.41291",
+      longitude: "-8.24389"
+    },
+    name: "Ireland"
+  },
+
   {
-      "country":"LI",
-       "coordinates":{
-              "latitude":"47.166","longitude":"9.555373"
-         },
-         "name":"Liechtenstein"  
- },
-  
+    country: "IS",
+    coordinates: {
+      latitude: "64.963051",
+      longitude: "-19.020835"
+    },
+    name: "Iceland"
+  },
   {
-      "country":"LU",
-       "coordinates":{
-              "latitude":"49.815273","longitude":"6.129583"
-         },
-         "name":"Luxembourg"  
- },
-  
+    country: "IT",
+    coordinates: {
+      latitude: "41.89277044",
+      longitude: "12.48366722"
+    },
+    name: "Italy"
+  },
+
   {
-      "country":"MC",
-       "coordinates":{
-              "latitude":"43.750298","longitude":"7.412841"
-         },
-         "name":"Monaco"  
- },
+    country: "LI",
+    coordinates: {
+      latitude: "47.166",
+      longitude: "9.555373"
+    },
+    name: "Liechtenstein"
+  },
+
   {
-      "country":"MD",
-       "coordinates":{
-              "latitude":"47.411631","longitude":"28.369885"
-         },
-         "name":"Moldova"  
- },
+    country: "LU",
+    coordinates: {
+      latitude: "49.815273",
+      longitude: "6.129583"
+    },
+    name: "Luxembourg"
+  },
+
   {
-      "country":"ME",
-       "coordinates":{
-              "latitude":"42.708678","longitude":"19.37439"
-         },
-         "name":"Montenegro"  
- },
- 
+    country: "MC",
+    coordinates: {
+      latitude: "43.750298",
+      longitude: "7.412841"
+    },
+    name: "Monaco"
+  },
   {
-      "country":"MT",
-       "coordinates":{
-              "latitude":"35.937496","longitude":"14.375416"
-         },
-         "name":"Malta"  
- },
- 
+    country: "MD",
+    coordinates: {
+      latitude: "47.411631",
+      longitude: "28.369885"
+    },
+    name: "Moldova"
+  },
   {
-      "country":"NL",
-       "coordinates":{
-              "latitude":"52.132633","longitude":"5.291266"
-         },
-         "name":"Netherlands"  
- },
+    country: "ME",
+    coordinates: {
+      latitude: "42.708678",
+      longitude: "19.37439"
+    },
+    name: "Montenegro"
+  },
+
   {
-      "country":"NO",
-       "coordinates":{
-              "latitude":"60.472024","longitude":"8.468946"
-         },
-         "name":"Norway"  
- },
-  
+    country: "MT",
+    coordinates: {
+      latitude: "35.937496",
+      longitude: "14.375416"
+    },
+    name: "Malta"
+  },
+
   {
-      "country":"PL",
-       "coordinates":{
-              "latitude":"51.919438","longitude":"19.145136"
-         },
-         "name":"Poland"  
- },
- 
+    country: "NL",
+    coordinates: {
+      latitude: "52.132633",
+      longitude: "5.291266"
+    },
+    name: "Netherlands"
+  },
   {
-      "country":"PT",
-       "coordinates":{
-              "latitude":"39.399872","longitude":"-8.224454"
-         },
-         "name":"Portugal"  
- },
- 
+    country: "NO",
+    coordinates: {
+      latitude: "60.472024",
+      longitude: "8.468946"
+    },
+    name: "Norway"
+  },
+
   {
-      "country":"RO",
-       "coordinates":{
-              "latitude":"45.943161","longitude":"24.96676"
-         },
-         "name":"Romania"  
- },
+    country: "PL",
+    coordinates: {
+      latitude: "51.919438",
+      longitude: "19.145136"
+    },
+    name: "Poland"
+  },
+
   {
-      "country":"RS",
-       "coordinates":{
-              "latitude":"44.016521","longitude":"21.005859"
-         },
-         "name":"Serbia"  
- },
- 
+    country: "PT",
+    coordinates: {
+      latitude: "39.399872",
+      longitude: "-8.224454"
+    },
+    name: "Portugal"
+  },
+
   {
-      "country":"SE",
-       "coordinates":{
-              "latitude":"60.128161","longitude":"18.643501"
-         },
-         "name":"Sweden"  
- },
- 
+    country: "RO",
+    coordinates: {
+      latitude: "45.943161",
+      longitude: "24.96676"
+    },
+    name: "Romania"
+  },
   {
-      "country":"SI",
-       "coordinates":{
-              "latitude":"46.151241","longitude":"14.995463"
-         },
-         "name":"Slovenia"  
- },
+    country: "RS",
+    coordinates: {
+      latitude: "44.016521",
+      longitude: "21.005859"
+    },
+    name: "Serbia"
+  },
+
   {
-      "country":"SK",
-       "coordinates":{
-              "latitude":"48.669026","longitude":"19.699024"
-         },
-         "name":"Slovakia"  
- },
+    country: "SE",
+    coordinates: {
+      latitude: "60.128161",
+      longitude: "18.643501"
+    },
+    name: "Sweden"
+  },
+
   {
-      "country":"SM",
-       "coordinates":{
-              "latitude":"43.94236","longitude":"12.457777"
-         },
-         "name":"San Marino"  
- },
- 
- 
+    country: "SI",
+    coordinates: {
+      latitude: "46.151241",
+      longitude: "14.995463"
+    },
+    name: "Slovenia"
+  },
   {
-      "country":"XK",
-       "coordinates":{
-              "latitude":"42.602636","longitude":"20.902977"
-         },
-         "name":"Kosovo"  
- }
- 	
-	];
+    country: "SK",
+    coordinates: {
+      latitude: "48.669026",
+      longitude: "19.699024"
+    },
+    name: "Slovakia"
+  },
+  {
+    country: "SM",
+    coordinates: {
+      latitude: "43.94236",
+      longitude: "12.457777"
+    },
+    name: "San Marino"
+  },
+
+  {
+    country: "XK",
+    coordinates: {
+      latitude: "42.602636",
+      longitude: "20.902977"
+    },
+    name: "Kosovo"
+  }
+];

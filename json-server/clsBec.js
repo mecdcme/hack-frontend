c49e2d3f14615d111e79db8892758ef552d81482
Francesco Amato <francamato@gmail.com>
2021-03-03 11:29:11 +0100

    add new cls

---
+++ json-server/clsBec.js
@@ -0,0 +1,23 @@
+module.exports = [
+   {
+    id: 1,
+    descr: "FOOD AND BEVERAGES"
+  },{
+    id: 2,
+    descr: "INDUSTRIAL SUPPLIES"
+  },
+   {
+    id: 3,
+    descr: "FUELS AND LUBRICANTS"
+  },{
+    id: 4,
+    descr: "CAPITAL GOODS"
+  },
+   {
+    id: 5,
+    descr: "TRANSPORT EQUIPMENT"
+  },{
+    id: 6,
+    descr: "CONSUMER GOODS"
+  } 
+];
